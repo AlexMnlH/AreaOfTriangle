d180c8f814206a5582052356bb595f971aac15d3
AlexMnlH <AlexMnlH>
2022-06-21 11:41:40 +0300

    Initial commit

---
+++ src/net/ukr/ahavrykin/Main.java
@@ -0,0 +1,8 @@
+package net.ukr.ahavrykin;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle1=new Triangle(5,4,4);
+        System.out.println(triangle1);
+    }
+}
