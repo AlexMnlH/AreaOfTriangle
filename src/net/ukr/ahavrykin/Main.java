b7d41d570dfb90aa570174e5155edfd306675cbd
AlexMnlH <AlexMnlH>
2022-06-21 13:01:23 +0300

    test commit 1

--- src/net/ukr/ahavrykin/Main.java
+++ src/net/ukr/ahavrykin/Main.java
@@ -2,7 +2,7 @@ package net.ukr.ahavrykin;
 
 public class Main {
     public static void main(String[] args) {
-        Triangle triangle1=new Triangle(5,4,4);
+        Triangle triangle1=new Triangle(6,4,4);
         System.out.println(triangle1);
     }
 }
